9a8b00d29ea21caf250b64b03c1bf3c21c521683
Manish <Manish@LAPTOP-U7HBVMA8>
2020-07-29 00:31:51 +0530

    first commit yes

---
+++ Elf1/src/test/java/Elf1/AppTest.java
@@ -0,0 +1,28 @@
+package Elf1;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+	public void testApp()
+    {
+        AssertJUnit.assertTrue( true );
+    }
+}
